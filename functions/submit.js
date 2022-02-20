148ae6115d2e6da20ea109ce86c167c5f75eb145
Hrishikesh Kokate <hrishi5200@gmail.com>
2022-02-20 13:45:21 +0530

    Added Akismet again

--- functions/submit.js
+++ functions/submit.js
@@ -1,52 +1,83 @@
+import {AkismetClient} from 'akismet-api'
 import {Client} from 'faunadb'
 import {Collection, Create} from 'faunadb/query'
 import {SMTPClient} from 'emailjs'
-export async function handler({body}) {
+export async function handler({headers, body}) {
   const {email, message, name, subject} = JSON.parse(body)
-  const {FAUNA, SMTP_PASSWORD, SMTP_RECEIVER, SMTP_USERNAME} = process.env
+  const {AKISMET, FAUNA, SMTP_PASSWORD, SMTP_RECEIVER, SMTP_USERNAME} = process.env
   function response(data, status) {
     return {
       body: JSON.stringify(data),
       statusCode: status
     }
   }
-  return new Client({
-    secret: FAUNA
-  }).query(Create(Collection('Submissions'), {
-    data: {
-      email,
-      message,
-      name,
-      subject
-    }
-  })).then(() => {
-    return new SMTPClient({
-      host: 'smtp-relay.sendinblue.com',
-      password: SMTP_PASSWORD,
-      port: 587,
-      tls: true,
-      user: SMTP_USERNAME
-    }).sendAsync({
-      from: `${name} <${email}>`,
-      'reply-to': `${name} <${email}>`,
-      subject,
-      text: message,
-      to: SMTP_RECEIVER
-    }).then(() => {
-      return response({
-        success: true
-      }, 200)
+  return new AkismetClient({
+    blog: 'https://www.tejalshinde.com/',
+    key: AKISMET
+  }).checkSpam({
+    content: message,
+    email,
+    ip: headers['x-nf-client-connection-ip'],
+    name,
+    referrer: headers['referrer'],
+    useragent: headers['user-agent']
+  }).then(spam => {
+    return new Client({
+      secret: FAUNA
+    }).query(Create(Collection('Submissions'), {
+      data: {
+        email,
+        message,
+        name,
+        spam,
+        subject
+      }
+    })).then(() => {
+      if (spam) {
+        return response({
+          spam,
+          success: true
+        }, 200)
+      } else {
+        return new SMTPClient({
+          host: 'smtp-relay.sendinblue.com',
+          password: SMTP_PASSWORD,
+          port: 587,
+          tls: true,
+          user: SMTP_USERNAME
+        }).sendAsync({
+          from: `${name} <${email}>`,
+          'reply-to': `${name} <${email}>`,
+          subject,
+          text: message,
+          to: SMTP_RECEIVER
+        }).then(() => {
+          return response({
+            spam,
+            success: true
+          }, 200)
+        }).catch(error => {
+          return response({
+            error,
+            spam,
+            stage: 'email',
+            success: false
+          }, 500)
+        })
+      }
     }).catch(error => {
       return response({
         error,
-        stage: 'email',
+        spam,
+        stage: 'database',
         success: false
       }, 500)
     })
   }).catch(error => {
     return response({
       error,
-      stage: 'database',
+      spam: false,
+      stage: 'spam',
       success: false
     }, 500)
   })
